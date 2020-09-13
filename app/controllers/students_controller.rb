994538e1a7078c1659f2e6cfe9872d672add5dba
ryancdavey1 <ryancdavey1@gmail.com>
2020-09-12 23:16:00 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,5 @@
 class StudentsController < ApplicationController
-  def create
-    @student = Student.new(student_params(:first_name, :last_name))
-    @student.save
-    redirect_to student_path(@student)
-  end
+  
 
   def show
     @student = Student.find(params[:id])
@@ -13,12 +9,18 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   def update
     @student = Student.find(params[:id])
     @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
-  
+
   def edit
     @student = Student.find(params[:id])
   end
